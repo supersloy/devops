b47200c75319a634372fa18a37507a4a3a8903e6
supersloy <51485198+supersloy@users.noreply.github.com>
2021-08-30 15:12:41 +0300

    fix: tests

---
+++ app_python/tests/test_app.py
@@ -0,0 +1,28 @@
+import datetime
+from flask.testing import FlaskClient
+
+
+def get_moscow_time():
+    timezone_info = datetime.timezone(datetime.timedelta(hours=+3))
+    current_time = datetime.datetime.now(timezone_info)
+    return current_time.strftime("%H:%M:%S")
+
+
+def test_status_codes(client: FlaskClient):
+    endpoints = ["/", "/api/time"]
+    for endpoint in endpoints:
+        assert client.get(endpoint).status_code == 200
+
+
+def test_correct_time_main_page(client: FlaskClient):
+    res = client.get("/")
+
+    expected_time = get_moscow_time()
+    assert expected_time in res.data.decode('ascii')
+
+
+def test_correct_time_data(client: FlaskClient):
+    res = client.get("/api/time")
+
+    expected_time = get_moscow_time()
+    assert expected_time == res.data.decode('ascii')
