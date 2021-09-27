5ed4ff361754126a26259902a0e910d94d505b9b
supersloy <51485198+supersloy@users.noreply.github.com>
2021-09-27 16:21:29 +0300

    devops(storage): 12 lab

--- app_python/src/__init__.py
+++ app_python/src/__init__.py
@@ -9,14 +9,22 @@ def create_app():
     def get_current_time(timezone_offset=3):
         timezone_info = datetime.timezone(datetime.timedelta(hours=timezone_offset))
         current_time = datetime.datetime.now(timezone_info)
-        return current_time.strftime("%H:%M:%S")
+        return current_time.strftime('%H:%M:%S')
 
     @app.route('/')
     def get_main_page():
-        return render_template("index.html", time=get_current_time())
+        f = open('/visits.txt','a')
+        f.write(f"{datetime.datetime.now(timezone_info)} \n")
+        return render_template('index.html', time=get_current_time())
 
     @app.route('/api/time')
     def get_time():
         return Response(get_current_time())
+
+    @app.route('/visits')
+    def visits():
+        f = open('/visits.txt','r')
+        return f.readlines()
+
 
     return app
